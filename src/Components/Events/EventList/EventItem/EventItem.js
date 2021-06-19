60e1c3432554b393fa0c9a06b22c3f70e40a3481
Jat15 <jinan15@hotmail.com>
2021-06-20 00:02:57 +0200

    Réécriture EventItem.js pour cohérence avec Events

--- src/Components/Events/EventList/EventItem/EventItem.js
+++ src/Components/Events/EventList/EventItem/EventItem.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import './EventItem.css'
 
-const eventItem = props => (
-    <li
-        key={props.eventId}
-        className="events__list-item"
-    >
-        <div>
-            <h2>{props.event_name}</h2>
-            <h3>{new Date(props.event_date).toLocaleString()}</h3>
-        </div>
-        <div>
-            {
-                props.userId === props.creatorId ?
-                    <p>Your the owner of the event</p> :
-                    <button
-                        className="btn"
-                        onClick={props.onDetail.bind(this, props.eventId)}
-                    >
-                        View details
-                    </button>
-            }
+export default function EventItem(props) {
+    return (
+        <li
+            className="events__list-item"
+        >
+            <div>
+                <h2>{props.event_name}</h2>
+                <h3>{new Date(props.event_date).toLocaleString()}</h3>
+            </div>
+            <div>
+                {
+                    props.userId === props.creatorId ?
+                        <p>Your the owner of the event</p> :
+                        <button
+                            className = "btn"
+                            onClick = {props.onDetail.bind(this, props.eventId)}
+                        >
+                            View details
+                        </button>
+                }
 
-        </div>
-    </li>
-)
-
-export default eventItem
+            </div>
+        </li>
+    )
+}
