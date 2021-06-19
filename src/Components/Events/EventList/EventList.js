147b341ce03c1a84362c715066de6bacecca9c3b
Jat15 <jinan15@hotmail.com>
2021-06-20 00:02:07 +0200

    Réécriture EventList.js pour cohérence avec Events

--- src/Components/Events/EventList/EventList.js
+++ src/Components/Events/EventList/EventList.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import './EventList.css'
 import EventItem from './EventItem/EventItem'
-const eventList = props => {
-    const events = props.events.map(event => {
-        return (
-            <EventItem
-                key={event._id}
-                eventId={event._id}
-                event_date={event.event_date}
-                event_name={event.event_name}
-                userId={props.authUserId}
-                creatorId={event.event_creator._id}
-                onDetail={props.onViewDetail}
-            />
-        )
-    })
 
+export default function EventList(props) {
     return (
-        <ul className="events__list">
-            {events}
+        <ul
+            className = "events__list"
+        >
+           {
+                props.events.map(event => 
+                    <EventItem
+                        key = {event._id}
+                        eventId = {event._id}
+                        event_date = {event.event_date}
+                        event_name = {event.event_name}
+                        userId = {props.authUserId}
+                        creatorId = {event.event_creator._id}
+                        onDetail = {props.onViewDetail}
+                    />
+                )
+           }
         </ul>
     )
 }
-
-export default eventList
