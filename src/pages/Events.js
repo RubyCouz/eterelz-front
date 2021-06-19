74c770c3e3c19d596a3e336ee3e7feb4becd1232
Jat15 <jinan15@hotmail.com>
2021-06-19 23:52:12 +0200

    Modification Events.js
    - Ajout d'un fragment pour la mutation et la query
    - Simplification d'écriture
    - Utilisation de useRef
    - Fermeture de la modal en confirment l'ajout
    - Correction de la fonction de la fermeture de la modal
    - Changement de mise en page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useEffect} from 'react'
+import React, {useState, useContext, useRef} from 'react'
 import AuthContext from '../context/auth-context'
 import Spinner from '../Components/Spinner/Spinner'
 import Modal from '../Components/Modal/Modal'
@@ -9,26 +9,32 @@ import {
     gql,
     useQuery,
     useMutation
-} from "@apollo/client";
+} from '@apollo/client'
 
-
+const EVENT_QUERY = gql`
+    fragment EventQuery on Event{
+        _id
+        event_name
+        event_date
+        event_desc
+        event_creator {
+            _id
+            user_email
+        }
+        createdAt
+    }
+`
 const LIST_EVENTS = gql`
+    ${EVENT_QUERY}
     query{
         events{
-            _id
-            event_name
-            event_date
-            event_desc
-            event_creator {
-                _id
-                user_email
-            }
-            createdAt
+            ...EventQuery
         }
     }
 `;
 
 const CREATE_EVENTS = gql`
+    ${EVENT_QUERY}
     mutation CreateEvent($event_name: String!, $event_date: String!, $event_desc: String!) {
         createEvent(eventInput: {
             event_name: $event_name
@@ -36,14 +42,14 @@ const CREATE_EVENTS = gql`
             event_desc: $event_desc
         })
         {
-            _id
+            ...EventQuery
         }
     }
 `;
 
-function EventsPage(props) {
-    const context = useContext(AuthContext)
 
+export default function EventsPage(props) {
+    const context = useContext(AuthContext)
 
     const [state, setState] = useState({
         creating: false,
@@ -52,13 +58,9 @@ function EventsPage(props) {
         selectedEvent: null
     })
   
-    const { loading, error, data } = useQuery(LIST_EVENTS)
-
     const modalCancelHandler = () => {
-        setState({
-            creating: false,
-            selectedEvent: null
-        })
+        const cancelModal = {creating: false,  selectedEvent: null}
+        setState({...state, ...cancelModal})
     }
 
     const startCreateEventHandler = () => {
@@ -70,92 +72,109 @@ function EventsPage(props) {
         setState({...state, selectedEvent: selectedEvent})
     }
 
-    const [input, setInput] = useState({
-        event_name: "",
-        event_desc: "",
-        event_date: ""
-    })
-    
-    function inputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.id;
-        setInput({ ...input, [name]: value });
-    }
+    const event_name = useRef('')
+    const event_desc = useRef('')
+    const event_date = useRef('')
+    const event_time = useRef('')
 
-    const [modalConfirmHandler, { dataCreateEvent }] = useMutation(
-        CREATE_EVENTS,{
-            variables:{
-                ...input
+    const { loading, error, data } = useQuery(LIST_EVENTS)
 
-            } 
+    const [modalConfirmHandler] = useMutation(
+        CREATE_EVENTS,
+        {
+            onCompleted: (dataMutationEvent) => {
+                if (dataMutationEvent.createEvent !== undefined){
+                    let listEvents = state.events
+
+                    listEvents.push(dataMutationEvent.createEvent)
+                    setState({...state, events : listEvents})
+
+                    modalCancelHandler()
+                }
+            }
         }
-        
-    );
+    )
 
     return ( 
         <>
             <h1>Liste des Events</h1>
-            {(state.creating || state.selectedEvent) && <BackDrop/>}
-            {state.creating &&
-                <Modal
-                    title="Création d'un event"
-                    canCancel
-                    canConfirm
-                    onCancel={modalCancelHandler}
-                    onConfirm={() => modalConfirmHandler()}
-                    confirmText="Confirm"
-                >
-                    <form action="">
-                        <div className="form_control">
-                            <label htmlFor="event_name">Nom de l'event</label>
-                            <input type="text" id="event_name" value={input.event_name} onChange={inputChange} />
-                        </div>
-                        <div className="form_control">
-                            <label htmlFor="event_desc">Description</label>
-                            <textarea name="event_desc" id="event_desc" cols="10" rows="4" value={input.event_desc} onChange={inputChange}/>
-                        </div>
-                        <div className="form_control">
-                            <label htmlFor="event_date">Date et Heure</label>
-                            <input type="datetime-local" id="event_date" value={input.event_date} onChange={inputChange}/>
-                        </div>
-
-                    </form>
-                </Modal>
+            {
+                (state.creating || state.selectedEvent) && 
+                    <BackDrop/>
             }
-            {state.selectedEvent && (
-                <Modal
-                    title={state.selectedEvent.event_name}
-                    canCancel
-                    canConfirm
-                    onCancel={modalCancelHandler}
-                    onConfirm={modalCancelHandler}
-                >
-                    <h2>{state.selectedEvent.event_name}</h2>
-                    <h3>{new Date(state.selectedEvent.event_date).toLocaleDateString()}</h3>
-                    <p>{state.selectedEvent.event_desc}</p>
-                </Modal>
-            )}
-            {context.token && (
-                <div className="events-control">
-                    <p>Créez Votre Event !!!</p>
-                    <button className="btn" onClick={startCreateEventHandler}>Création d'event</button>
-                </div>
-            )}
+            {   
+                state.creating &&
+                    <Modal
+                        title = "Création d'un event"
+                        canCancel
+                        canConfirm
+                        onCancel = {modalCancelHandler}
+                        onConfirm = {
+                            () => modalConfirmHandler({
+                                variables:{
+                                    event_name: event_name.current.value,
+                                    event_date: event_date.current.value + "T" + event_time.current.value,
+                                    event_desc: event_desc.current.value
+                                }
+                            })
+                        }
+                        confirmText = "Confirm"
+                    >
+                        <form action="">
+                            <div className="form_control">
+                                <label htmlFor="event_name">Nom de l'event</label>
+                                <input type="text" id="event_name" ref={event_name}/>
+                            </div>
+                            <div className="form_control">
+                                <label htmlFor="event_desc">Description</label>
+                                <textarea name="event_desc" id="event_desc" cols="10" rows="4" ref={event_desc}/>
+                            </div>
+                            <div className="form_control">
+                                <label htmlFor="event_date">Date</label>
+                                <input type="date" id="event_date" ref={event_date} />
+                            </div>
+                            <div className="form_control">
+                                <label htmlFor="event_time">Heure</label>
+                                <input type="time" id="event_time" ref={event_time} />
+                            </div>
+
+                        </form>
+                    </Modal>
+            }
+            {
+                state.selectedEvent &&
+                    <Modal
+                        title = {state.selectedEvent.event_name}
+                        canCancel
+                        canConfirm
+                        onCancel = {modalCancelHandler}
+                        onConfirm = {modalCancelHandler}
+                    >
+                        <h2>{state.selectedEvent.event_name}</h2>
+                        <h3>{new Date(state.selectedEvent.event_date).toLocaleDateString()}</h3>
+                        <p>{state.selectedEvent.event_desc}</p>
+                    </Modal>
+            }
+            {
+                context.token &&
+                    <div className="events-control">
+                        <p>Créez Votre Event !!!</p>
+                        <button className="btn" onClick={startCreateEventHandler}>Création d'event</button>
+                    </div>
+            }
             <section>
-                 { loading ?
+                { 
+                    loading ?
                         <Spinner/>:
                         error ?
-                        <p>{JSON.stringify(error)}</p>
-                        :<EventList
-                            events={data.events}
-                            authUserId={context.playload ? context.playload.userId : null}
-                            onViewDetail={showDetailHandler}
-                        />
-                    }
+                            <p>{JSON.stringify(error)}</p>:
+                            <EventList
+                                events={state.events.concat(data.events)}
+                                authUserId={context.playload ? context.playload.userId : null}
+                                onViewDetail={showDetailHandler}
+                            />
+                }
             </section>
         </>
-        )
+    )
 }
-
-export default EventsPage
