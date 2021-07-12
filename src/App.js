3e58d20c2819989e8b505a9737321af015c8bfe9
Jat15 <jinan15@hotmail.com>
2021-07-12 14:47:40 +0200

    Modification App.js supression des anciens import

--- src/App.js
+++ src/App.js
@@ -14,35 +14,14 @@ import EventsPage from './pages/Events'
 import BackOffice from './pages/BackOffice'
 import Navbar from './Components/Navbar/Navbar'
 
+import {
+    ThemeProvider,
+    createMuiTheme,
+    makeStyles
+} from '@material-ui/core/styles'
 
-
-
-import clsx from 'clsx';
-import {} from '@material-ui/core/styles';
-import { ThemeProvider, createMuiTheme, makeStyles, useTheme } from '@material-ui/core/styles'
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import AccountBoxTwoToneIcon from '@material-ui/icons/AccountBoxTwoTone';
-import EventNoteTwoToneIcon from '@material-ui/icons/EventNoteTwoTone';
-import ExitToAppTwoToneIcon from '@material-ui/icons/ExitToAppTwoTone';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 import './App.css'
-import {Button, MenuItem} from "@material-ui/core";
-import {AccountCircle} from "@material-ui/icons";
 
 const theme = createMuiTheme({
     palette: {
@@ -73,7 +52,7 @@ const theme = createMuiTheme({
     },
     content: {
       flexGrow: 1,
-      padding: theme.spacing(4, 0, 0 , 0)
+      padding: theme.spacing(0, 0, 0 , 0)
     },
   }));
   
