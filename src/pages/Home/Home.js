8939d03b84da3bc126cb4904d62abc27032e4cb7
RubyCouz <cousin.ruby.cedric@gmail.com>
2021-10-11 10:02:43 +0200

    début refonte navbar accueil

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,9 +6,9 @@ import {
     Tabs,
     AppBar,
     MenuItem,
-    Toolbar,
+    Toolbar, Menu,
 } from "@material-ui/core"
-import {AccountCircle as AccountCircleIcon} from "@material-ui/icons"
+import {AccountCircle, AccountCircle as AccountCircleIcon, Search} from "@material-ui/icons"
 import {makeStyles} from '@material-ui/core/styles'
 
 import SwipeableViews from 'react-swipeable-views'
@@ -16,6 +16,17 @@ import SwipeableViews from 'react-swipeable-views'
 import {NavLink} from "react-router-dom"
 
 import TabPanel from './TabPanel'
+import Box from "@mui/material/Box"
+import Badge from "@material-ui/core/Badge"
+import MailIcon from "@material-ui/icons/Mail"
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import MenuIcon from "@material-ui/icons/Menu"
+import Typography from "@mui/material/Typography"
+import {alpha} from "@mui/material/styles"
+import styled from "@emotion/styled"
+import MoreIcon from "@material-ui/icons/MoreVert"
+import SearchIcon from "@material-ui/icons/Search";
+import * as PropTypes from "prop-types";
 
 function a11yProps(index) {
     return {
@@ -42,7 +53,8 @@ const useStyles = makeStyles((theme) => ({
 export default function Home(props) {
     const classes = useStyles();
     const [value, setValue] = useState(0);
-
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
     const handleChange = (event, newValue) => {
         setValue(newValue);
     }
@@ -51,9 +63,103 @@ export default function Home(props) {
         setValue(index);
     }
 
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    }
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    }
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    }
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={4} color="error">
+                        <MailIcon/>
+                    </Badge>
+                </IconButton>
+                <p>Messages</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    size="large"
+                    aria-label="show 17 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={17} color="error">
+                        <NotificationsIcon/>
+                    </Badge>
+                </IconButton>
+                <p>Notifications</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle/>
+                </IconButton>
+                <p>Profile</p>
+            </MenuItem>
+        </Menu>
+    );
+
     return (
-        <>
-            <div className={classes.root}>
+        <div>
+            <Box sx={{flexGrow: 1}}>
                 <AppBar
                     position="static"
                     color="primary"
@@ -66,33 +172,50 @@ export default function Home(props) {
                                 className={classes.logo}
                             />
                         </NavLink>
-                        <Tabs
-                            value={value}
-                            onChange={handleChange}
-                            indicatorColor="secondary"
-                            textColor="inherit"
-                            variant="fullWidth"
-                            aria-label="full width tabs example"
-                        >
-                            <Tab label="Tournois / Event" {...a11yProps(0)} />
-                            <Tab label="Les Streams" {...a11yProps(1)} />
-                            <Tab label="Jeux" {...a11yProps(2)} />
-                            <Tab label="La commu EterelZ" {...a11yProps(3)} />
-                        </Tabs>
-                        <MenuItem>
-                            <NavLink to="/auth">
-                                <IconButton
-                                    aria-label="account of current user"
-                                    aria-controls="primary-search-account-menu"
-                                    aria-haspopup="true"
-                                    color="inherit"
-                                >
-                                    <AccountCircleIcon/>
-                                </IconButton>
-                            </NavLink>
-                        </MenuItem>
+                        <Box sx={{flexGrow: 1}}>
+                            <Tabs
+                                value={value}
+                                onChange={handleChange}
+                                indicatorColor="secondary"
+                                textColor="inherit"
+                                variant="fullWidth"
+                                aria-label="full width tabs example"
+                            >
+                                <Tab label="Tournois / Event" {...a11yProps(0)} />
+                                <Tab label="Les Streams" {...a11yProps(1)} />
+                                <Tab label="Jeux" {...a11yProps(2)} />
+                                <Tab label="La commu EterelZ" {...a11yProps(3)} />
+                            </Tabs>
+                        </Box>
+                        <Box sx={{display: {xs: 'none', md: 'flex'}}}>
+                            <IconButton
+                                size="large"
+                                edge="end"
+                                aria-label="account of current user"
+                                aria-controls={menuId}
+                                aria-haspopup="true"
+                                onClick={handleProfileMenuOpen}
+                                color="inherit"
+                            >
+                                <AccountCircle/>
+                            </IconButton>
+                        </Box>
+                        <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+                            <IconButton
+                                size="large"
+                                aria-label="show more"
+                                aria-controls={mobileMenuId}
+                                aria-haspopup="true"
+                                onClick={handleMobileMenuOpen}
+                                color="inherit"
+                            >
+                                <MoreIcon/>
+                            </IconButton>
+                        </Box>
                     </Toolbar>
                 </AppBar>
+                {renderMobileMenu}
+                {renderMenu}
                 <SwipeableViews
                     axis={props.theme === 'rtl' ? 'x-reverse' : 'x'}
                     index={value}
@@ -111,7 +234,8 @@ export default function Home(props) {
                         Page présentation commu EterelZ
                     </TabPanel>
                 </SwipeableViews>
-            </div>
-        </>
+            </Box>
+        </div>
+
     )
 }
