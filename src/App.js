e5f6f8ca64f63c452467fa1bbbc79b78dc4170ca
RubyCouz <cousin.ruby.cedric@gmail.com>
2021-11-17 20:45:40 +0100

    fix widget discord

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, {Suspense, lazy, useState, useEffect} from 'react'
 import Loading from './pages/Loading'
+
 const Index = lazy(() => import('./pages/Index'))
 
 export default function App() {
 
     let [crate, setCrate] = useState()
 
-    useEffect(async () => {
+    useEffect(() => {
+        async function widget() {
+            let result = await import('@widgetbot/crate')
+            const Crate = await result.cdn();
 
-        let result = await import('@widgetbot/crate')
-        const Crate = await result.cdn();
-
-        crate = new Crate({
-            server: '326099902037884940',
-            channel: '670890139438022687',
-            location: ['bottom', 'right'],
-            notifications: true,
-            indicator: true,
-        })
+            crate = new Crate({
+                server: '326099902037884940',
+                channel: '670890139438022687',
+                location: ['bottom', 'right'],
+                notifications: true,
+                indicator: true,
+            })
+        }
+        widget()
     })
 
     return (
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading/>}>
             {crate}
             <Index/>
         </Suspense>
