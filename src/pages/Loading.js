9e3344c22ef0ab8a500bc040b56d02182c92d36f
RubyCouz <cousin.ruby.cedric@gmail.com>
2021-12-11 10:50:21 +0100

    fix sponso hl, add loading logo

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {
-    CircularProgress,
     Box,
 } from '@material-ui/core'
 
 export default function Loading() {
     return (
         <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-            {/*<CircularProgress />*/}
             <img src="./img/eterelz/logo/loading.gif" alt="logo de chargement" title="chargement de la page"/>
         </Box>
     )
