bf6e0ff6c725a9061c8e98366562cbdc3dccdaf3
RubyCouz <cousin.ruby.cedric@gmail.com>
2022-01-03 16:29:12 +0100

    test api

--- src/context/apollo-context.js
+++ src/context/apollo-context.js
@@ -4,7 +4,7 @@ import {
 } from '@apollo/client'
 
 export const graphqlConfig = new ApolloClient({
-    uri: '/api',
+    uri: 'http://178.128.42.251/api',
     // uri: 'https://api.rubycouz.xyz/api',
     credentials: 'include',
     cache: new InMemoryCache(),
