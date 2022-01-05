acaf61a3750cdbec0237bb9e9eebe6b910f77219
RubyCouz <cousin.ruby.cedric@gmail.com>
2022-01-05 18:25:28 +0100

    test router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter basename={"/react-routing-config"}>
+      <BrowserRouter basename={"/"}>
           <App />
       </BrowserRouter>
   </React.StrictMode>,
