637fda0c1e4aea3e3e4a1dbe0c93d6ed41b27270
RubyCouz <cousin.ruby.cedric@gmail.com>
2022-01-14 17:27:57 +0100

    fix socket

--- src/context/socket-context.js
+++ src/context/socket-context.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {io} from 'socket.io-client'
-import {HOST} from '../config'
+import {SOCKET_HOST} from '../config'
 
-export const socket = io(HOST, {
+export const socket = io(SOCKET_HOST, {
     transports: ["websocket"]
 })
 export const SocketContext = React.createContext(socket)
